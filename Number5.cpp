1b9871c8a09f47a63e15b5d9fa945be1d6793596
andremcmiller <50882269+andremcmiller@users.noreply.github.com>
2020-11-19 14:55:18 +0300

    Update Number5.cpp

--- Number5.cpp
+++ Number5.cpp
@@ -1,3 +1,5 @@
+//Код прислал https://vk.com/fuckaiieasyidareblocked123456789
+//Чуть нижуу будет мой код, исправленый
 #include <iostream>
 using namespace std;
 
@@ -7,5 +9,18 @@ int main()
     cin >> c;
     int n = c * 2, m = c * 9;
     cout << n / 11 << " " << n % 11 << "/11 kg Ni  " << m / 11 << " " << m % 11 << "/11 kg Cu";
+    return 0;
+}
+
+//Мой код:
+#include <iostream>
+using namespace std;
+int main()
+{
+    float c;
+    cout << "Enter C: ";
+    cin >> c;
+    float n = c * 2, m = c * 9;
+    cout << n / 11  << " kg Ni \n" << "\n" << m / 11 << " kg Cu";
     return 0;
 }
