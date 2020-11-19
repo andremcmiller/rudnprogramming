9f516b94109cab7394c7b623352fea3b0fcfbcd9
andremcmiller <50882269+andremcmiller@users.noreply.github.com>
2020-11-19 16:55:02 +0300

    Update Number7.cpp

--- Number7.cpp
+++ Number7.cpp
@@ -15,3 +15,19 @@ int main()
     cout << t << " Hours " << minutes << " Minutes " << seconds << " Seconds";
     return 0;
 }
+
+//                 Мой код
+
+
+#include <stdio.h>
+int main()
+{
+    int t ;
+    scanf("%d",&t);
+    int seconds = t % 60;
+    t /= 60;
+    int minutes = t % 60;
+    t /= 60;
+    printf(" %d Hours\n %d Minutes\n %d Seconds\n",t,minutes,seconds);
+    return 0;
+}
