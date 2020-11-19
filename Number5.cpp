ee4e5f54dec0c7dbbdce4975a919f267f6998a15
andremcmiller <50882269+andremcmiller@users.noreply.github.com>
2020-11-19 14:27:57 +0300

    Update Number5.cpp

--- Number5.cpp
+++ Number5.cpp
@@ -6,6 +6,6 @@ int main()
     int c;
     cin >> c;
     int n = c * 2, m = c * 9;
-    cout << n / 11 << " " << n % 11 << "/11 kg nicelya  " << m / 11 << " " << m % 11 << "/11 kg medi";
+    cout << n / 11 << " " << n % 11 << "/11 kg Ni  " << m / 11 << " " << m % 11 << "/11 kg Cu";
     return 0;
 }
